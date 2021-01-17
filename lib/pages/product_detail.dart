eb35c4be2926cb726562ca55a6e23e76a670577d
Ryo Hirano <ryo.hirano@monocrea.co.jp>
2021-01-17 14:56:57 +0900

    feat: fulfilling screen detail.

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -13,9 +13,49 @@ class ProductDetail extends StatelessWidget {
         title: Text("商品詳細"),
       ),
       body: Container(
-        child: Center(
-          child: Text(product.title),
-        ),
+        child: _body(context, product),
+      ),
+    );
+  }
+
+  Widget _body(BuildContext context, Product product) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Center(child: Image.network(product.sampleImageUrl)),
+          Text(
+            product.title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(product.item.humanizeName),
+          Text("${product.price.toString()}円"),
+          Text("作成者: ${product.material.user.name}"),
+          product.material.description.isEmpty
+              ? Container()
+              : _descriptionSection(context, product),
+        ],
+      ),
+    );
+  }
+
+  Widget _descriptionSection(BuildContext context, Product product) {
+    return Container(
+      margin: EdgeInsets.only(top: 16.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "このアイテムについて",
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(16.0),
+            child: Text(product.material.description),
+          )
+        ],
       ),
     );
   }
